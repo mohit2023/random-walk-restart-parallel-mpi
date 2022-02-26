a489e35c39e8d4a3030de517961723f302b96a77
Mohit Sharma <mohit7153@gmail.com>
2022-02-26 13:20:59 +0530

    reduce files

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include "io.h"
-#include "randomWalk.h"
 #include <assert.h>
+#include "randomizer.hpp"
 
 using namespace std;
+
+void randomWalk(int node, int **adj, int *num_child, int *count, int num_steps, Randomizer r, int user_node) {
+    int u = node;
+    count[u]++;
+    while(num_steps>0) {
+        int childCount = num_child[u];
+        if(childCount>0) {
+            int next_step = r.get_random_value(user_node);
+            if(next_step<0) {
+                u = node;
+            } else {
+                int next_index = next_step%childCount;
+                u = adj[u][next_index];
+            }
+        } else {
+            u = node;
+        }
+        count[u]++;
+        num_steps--;
+    }
+}
 
 int main(int argc, char* argv[]){
 
--- randomWalk.h
+++
@@ -1,3 +0,0 @@
-#include "randomizer.hpp"
-
-void randomWalk(int node, int **adj, int* num_child, int *count, int num_steps, Randomizer r, int user_node);
