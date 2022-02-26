8d98e7dbe23e573428b3ac11f29a3d66f7f9e247
Mohit Sharma <mohit7153@gmail.com>
2022-02-26 13:11:18 +0530

    scripts

--- run.sh
+++ run.sh
@@ -1,15 +1,19 @@
-mpirun -np $1 ./wtf data/8717_31525/edges.dat 8717 31525 0.1 5000 30 20 369 > $1time1.txt
-echo break >> $1time1.txt
-mpirun -np $1 ./wtf data/8717_31525/edges.dat 8717 31525 0.1 5000 30 20 369 >> $1time1.txt
-echo break >> $1time1.txt
-mpirun -np $1 ./wtf data/8717_31525/edges.dat 8717 31525 0.1 5000 30 20 369 >> $1time1.txt
-mpirun -np $1 ./wtf data/81867_545671/edges.dat 81867 545671 0.1 5000 30 20 369 > $1time2.txt
-echo break >> $1time2.txt
-mpirun -np $1 ./wtf data/81867_545671/edges.dat 81867 545671 0.1 5000 30 20 369 >> $1time2.txt
-echo break >> $1time2.txt
-mpirun -np $1 ./wtf data/81867_545671/edges.dat 81867 545671 0.1 5000 30 20 369 >> $1time2.txt
-mpirun -np $1 ./wtf data/82168_870161/edges.dat 82168 870161 0.1 5000 30 20 369 > $1time3.txt
-echo break >> $1time3.txt
-mpirun -np $1 ./wtf data/82168_870161/edges.dat 82168 870161 0.1 5000 30 20 369 >> $1time3.txt
-echo break >> $1time3.txt
-mpirun -np $1 ./wtf data/82168_870161/edges.dat 82168 870161 0.1 5000 30 20 369 >> $1time3.txt
+if [ "$2"=="1" ]; then
+    mpirun -np $1 ./wtf data/8717_31525/edges.dat 8717 31525 0.1 5000 30 20 369 > $1time1.txt
+    echo break >> $1time1.txt
+    mpirun -np $1 ./wtf data/8717_31525/edges.dat 8717 31525 0.1 5000 30 20 369 >> $1time1.txt
+    echo break >> $1time1.txt
+    mpirun -np $1 ./wtf data/8717_31525/edges.dat 8717 31525 0.1 5000 30 20 369 >> $1time1.txt
+elif [ "$2"=="2" ]; then
+    mpirun -np $1 ./wtf data/81867_545671/edges.dat 81867 545671 0.1 5000 30 20 369 > $1time2.txt
+    echo break >> $1time2.txt
+    mpirun -np $1 ./wtf data/81867_545671/edges.dat 81867 545671 0.1 5000 30 20 369 >> $1time2.txt
+    echo break >> $1time2.txt
+    mpirun -np $1 ./wtf data/81867_545671/edges.dat 81867 545671 0.1 5000 30 20 369 >> $1time2.txt
+else
+    mpirun -np $1 ./wtf data/82168_870161/edges.dat 82168 870161 0.1 5000 30 20 369 > $1time3.txt
+    echo break >> $1time3.txt
+    mpirun -np $1 ./wtf data/82168_870161/edges.dat 82168 870161 0.1 5000 30 20 369 >> $1time3.txt
+    echo break >> $1time3.txt
+    mpirun -np $1 ./wtf data/82168_870161/edges.dat 82168 870161 0.1 5000 30 20 369 >> $1time3.txt
+fi
